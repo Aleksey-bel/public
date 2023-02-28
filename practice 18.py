366ffee70976ab2b4b3762f067f9ec35c6beb2bb
Aleksey-bel <123276271+Aleksey-bel@users.noreply.github.com>
2023-03-01 01:10:21 +0300

    Add files via upload
    Для онлайн-конференции необходимо написать программу, которая будет подсчитывать общую стоимость билетов. Программа должна работать следующим образом:
    
    1. В начале у пользователя запрашивается количество билетов, которые он хочет приобрести на мероприятие.
    
    2. Далее для каждого билета запрашивается возраст посетителя, в соответствии со значением которого выбирается стоимость:
    
    Если посетителю конференции менее 18 лет, то он проходит на конференцию бесплатно.
    От 18 до 25 лет — 990 руб.
    От 25 лет — полная стоимость 1390 руб.
    3. В результате программы выводится сумма к оплате. При этом, если человек регистрирует больше трёх человек на конференцию, то дополнительно получает 10% скидку на полную стоимость заказа.

---
+++ practice 18.py
@@ -0,0 +1,22 @@
+number_of_tickets = int(input("Введите количество билетов:"))
+age = []
+total_cost = 0
+if number_of_tickets > 10:
+    print("К сожалению, Вы не можете приобрести более 10 билетов в одном заказе!")
+    print("-------")
+    print("Попробуйте снова=)")
+else:
+    for i in range(0, number_of_tickets):
+        age = int(input(f"Введите возраст посетителя #{i+1}:"))
+
+        if age < 18:
+            print("Вход бесплатный!")
+        elif 18 <= age < 25:
+            total_cost += 990
+        else:
+            total_cost += 1390
+    if number_of_tickets > 3:
+        total_cost = total_cost * 0.9
+        print('Итого со скидкой 10% к оплате:', total_cost)
+    else:
+        print('Итого к оплате:', total_cost)
